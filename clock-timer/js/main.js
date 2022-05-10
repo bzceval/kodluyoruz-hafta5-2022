c599d7cbfa284daefef11dc47d8d137aa505446a
bceval <busraaceval@gmail.com>
2022-05-10 16:03:17 +0300

    crated js file and maked functions for template

---
+++ clock-timer/js/main.js
@@ -0,0 +1,22 @@
+let userName = document.getElementById("myName")
+let clock = document.getElementById("myClock")
+let weekDay = ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"]
+
+userName = prompt("Adınız nedir?")
+
+
+function showTime() {
+  const today = new Date()
+  let h = today.getHours()
+  let m = today.getMinutes()
+  let s = today.getSeconds()
+  let d = weekDay[today.getDay()]
+
+
+  myName.innerHTML = `Hoşgeldin ${userName}`
+  clock.innerHTML = h + ":" + m + ":" + s + " " + d
+
+  setTimeout(showTime, 1000)
+}
+
+showTime()
